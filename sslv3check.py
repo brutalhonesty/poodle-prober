c3ef6d3c17464256d537ed420361a9e1b9ed4407
Adam Schodde <aaschodd@asu.edu>
2014-12-12 15:58:10 -0700

    Failing script.

--- sslv3check.py
+++ sslv3check.py
@@ -7,15 +7,49 @@ python3 sslcheck.py 10.0.1.0/24
 
 jcmurphy@buffalo.edu
 """
-import socket, ssl, pprint, sys, IPy, argparse, multiprocessing
+import socket, ssl, pprint, sys, IPy, argparse, multiprocessing, csv, os
 
 parser = argparse.ArgumentParser(description='Scan a netblock for SSLv3 enabled servers on port 443')
 parser.add_argument('--port', '-p', nargs='*', default=["443"], help='port to connect to (default=443)')
 group = parser.add_mutually_exclusive_group(required=True)
-group.add_argument('--network', '-n', nargs='*', default=None, help='<network/mask>')
+group.add_argument('--network', '-n', nargs='*', default=None, help='<network/args>')
+group.add_argument('--input', '-i', default=None, help='input CSV file')
 group.add_argument('--host', '-H', nargs='*', default=None, help='hostname')
+parser.add_argument('--output', '-o', default=None, help='output CSV file')
 parser.add_argument('--tls', '-t', action='store_true', default=False, help='check if SSLv3 is enabled and TLSv1 is not enabled\n otherwise just see if SSLv3 is enabled')
 parser.add_argument('--parallel', '-P', action='store_true', default=False, help='Process netblocks in parallel')
+parser.add_argument('--verbose', '-v', action='store_true', default=False, help='Enable verbosity of output to stdout.')
+
+
+def _validate_input(input_file):
+    input_file = os.path.abspath(input_file)
+    if os.path.exists(input_file) is False:
+        raise Exception('Input file does not exist.')
+    return input_file
+
+
+def _output_ips(results):
+    with open('ips.txt', 'w') as file:
+        for result in results:
+            file.write(str(result['host']) + '\n')
+        file.close()
+
+
+def output_csv(tls_enabled, results, output_file):
+    with open(output_file, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        if tls_enabled:
+            writer.writerow(['IP Address', 'Port', 'SSL', 'TLS'])
+        else:
+            writer.writerow(['IP Address', 'Port', 'SSL'])
+        for result in results:
+            if tls_enabled is False:
+                writer.writerow([str(result['host']), result['port'], str(result['sslv3'])])
+            elif result['sslv3'] == "enabled" and result['tlsv1'] != "enabled":
+                writer.writerow([str(result['host']), result['port'], "enabled", "not enabled"])
+            else:
+                writer.writerow([str(result['host']), result['port'], str(result['sslv3']), str(result['tlsv1'])])
+        csvfile.close()
 
 
 def print_results(host, port, sslv3, tlsv1):
@@ -27,6 +61,7 @@ def print_results(host, port, sslv3, tlsv1):
         print("{0}:{1} SSLv3 enabled and TLSv1 not enabled".format(str(host), port))
     else:
         print("{0}:{1} SSLv3={2} TLSv1={3}".format(str(host), port, sslv3, tlsv1))
+
 
 def main():
     args = parser.parse_args()
@@ -45,22 +80,30 @@ def main():
         for host in args["host"]:
             for p in args["port"]:
                 sslv3 = check_sslv3(host, p)
-                if args["tls"] == True:
+                if args["tls"] is True:
                     tlsv1 = check_tls(host, p)
                 print_results(host, p, sslv3, tlsv1)
         return
 
     net = IPy.IPSet()
-
-    for network in args["network"]:
-        net.add(IPy.IP(network))
+    if args['network']:
+        for network in args['network']:
+            net.add(IPy.IP(network))
+    elif args['input']:
+        input_file = _validate_input(args['input'])
+        with open(input_file, newline='') as csvfile:
+            reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+            for row in reader:
+                net.add(IPy.IP('\n'.join(row)))
+    else:
+        raise Exception('Missing network or input file.')
 
     if args["parallel"]:
         p = multiprocessing.Pool()
         q = multiprocessing.Queue()
 
         for ip in net:
-            q.put((check_net, ip, args["port"], args["tls"]))
+            q.put((check_net, ip, args["port"], args["tls"], args["verbose"]))
 
         while True:
             items = q.get()
@@ -72,25 +115,35 @@ def main():
                 p.join()
                 break
     else:
+        results = []
         for ip in net:
-            check_net(ip, args["port"], args["tls"])
+            checked_values = check_net(ip, args["port"], args["tls"], args["verbose"])
+            results.append(checked_values)
+        if args['output']:
+            output_csv(args['tls'], results, args['output'])
 
-def check_net(ip, ports, tls):
+
+def check_net(ip, ports, tls, is_verbose):
     for x in ip:
-        if ip.prefixlen() != 32 and (ip.broadcast() == x or ip.net() == x):
-            continue
+        # if ip.prefixlen() != 32 and (ip.broadcast() == x or ip.net() == x):
+        #    return {'host': x, 'port': '443', 'sslv3': 'UNKNOWN', 'tlsv1': 'UNKNOWN'}
         for p in ports:
             tlsv1 = None
             sslv3 = check_sslv3(x, p)
-            if tls == True:
+            if tls is True:
                 tlsv1 = check_tls(x, p)
-            print_results(x, p, sslv3, tlsv1)
+            if is_verbose is True:
+                print_results(x, p, sslv3, tlsv1)
+            return {'host': x, 'port': p, 'sslv3': sslv3, 'tlsv1': tlsv1}
+
 
 def check_tls(h, p):
     return check(h, p, ssl.PROTOCOL_TLSv1)
 
+
 def check_sslv3(h, p):
     return check(h, p, ssl.PROTOCOL_SSLv3)
+
 
 def check(h, p, ctx):
     context = ssl.SSLContext(ctx)
